1d76adc8cbf121a057fcfde02b32c58acf19468e
Experyt <kf_kf_95@mail.ru>
2018-12-20 18:39:33 +0300

    initial commit

---
+++ practic1/date.php
@@ -0,0 +1,39 @@
+<?
+return[ 
+	'about' => [
+		'name' => 'Александр Ковязин',
+		'photo' => '',
+		'email' => 'kf_kf_95@mail.ru',
+		'phone' => '+7(912)375-76-36',
+		'site' => 'example.com',
+		'post' => 'Team Lead PHP',
+	],
+	'education' => [
+	[
+		'title' => 'Школа',
+		'speciality' => 'Школьник',
+		'yearStart' => '2007',
+		'yearEnd' => '2016',
+	],
+	[	'title' => 'Техникум',
+		'speciality' => 'Студент',
+		'yearStart' => '2016',
+		'yearEnd' => '2020',
+	]
+   ],
+	'languages' => [
+		[
+			'title' => 'English',
+			'level' => 'Intermediate C2',
+		],
+		[
+			'title' => 'Russian',
+			'level' => 'Intermediate C4',
+		],
+	],
+		'interest' => [
+		'Сладкое',
+		'Мотоциклы',
+		'Программированние',
+	],
+];
